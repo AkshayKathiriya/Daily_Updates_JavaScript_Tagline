90e9302c45a4e304234f1d8812e21d7381e4bc19
AkshayKathiriya <85862017+AkshayKathiriya@users.noreply.github.com>
2021-08-20 13:09:07 +0530

    1

---
+++ alert.js
@@ -0,0 +1,13 @@
+function hello(name) {
+  let phrase = `Hello, ${name}!`;
+
+  // <-- the debugger stops here
+
+  alert(phrase);
+}
+
+hello("surprise");
+
+function newFunction(){
+  alert("And you are heree")
+}
